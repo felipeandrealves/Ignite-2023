544fcf7d6e60d2077bf5aa43a32225ea41f98017
Felipe Alves <felipe_alves_sbo@hotmail.com>
2023-06-15 23:48:12 -0300

    feat: use default layout

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,11 +1,14 @@
 import { Route, Routes } from 'react-router-dom'
 
+import { DefaultLayout } from './common/layouts/DefaultLayout'
 import { Home } from './pages/Home'
 
 export const Router = () => {
   return (
     <Routes>
+      <Route path="/" element={<DefaultLayout />}>
       <Route path="/" element={<Home />} />
+      </Route>
     </Routes>
   )
 }
