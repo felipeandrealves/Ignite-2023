248b9ce64b63583e10b50669a109caa8cc28374f
Felipe Alves <felipe_alves_sbo@hotmail.com>
2023-06-15 23:47:18 -0300

    feat: finish home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,95 +1,30 @@
 import {
-  ArchiveBox,
-  Coffee,
-  MapPin,
-  Minus,
-  Plus,
-  ShoppingCart,
-  Timer,
-} from 'phosphor-react'
-import { Link } from 'react-router-dom'
-
-import {
-  HeaderContainer,
-  Location,
   IntroSectionContainer,
-  ShoppingCartButton,
-  AdvantagesItems,
-  Intro,
-  Advantage,
   CoffeeList,
   CoffeeListContainer,
-  CoffeeCard,
-  CoffeeCardFooter,
-  Quantity,
+  Intro,
+  IntroTitle,
 } from './styles'
-import CoffeeDeliveryLogo from '../../common/assets/images/CoffeeDeliveryLogo.svg'
-import CoffeeCup from '../../common/assets/images/CoffeeCup.svg'
+import { AdvantageList } from './components/AdvantageList'
+import { CoffeeCard } from './components/CoffeeCard'
 
-import CoffeeSample from '../../common/assets/images/coffees/Image.svg'
+import CoffeeCup from '../../common/assets/images/CoffeeCup.svg'
 
 export const Home = () => {
   return (
     <div>
-      <HeaderContainer>
-        <Link to="/">
-          <img src={CoffeeDeliveryLogo} alt="" />
-        </Link>
-
-        <nav>
-          <Location>
-            <MapPin size={22} weight="fill" />
-            <p>Porto Alegre, RS</p>
-          </Location>
-
-          <ShoppingCartButton>
-            <ShoppingCart size={22} weight="fill" />
-          </ShoppingCartButton>
-        </nav>
-      </HeaderContainer>
-
       <IntroSectionContainer>
         <Intro>
-          <h1>Encontre o café perfeito para qualquer hora do dia</h1>
+          <IntroTitle>
+            <h1>Encontre o café perfeito para qualquer hora do dia</h1>
 
-          <p>
-            Com o Coffee Delivery você recebe seu café onde estiver, a qualquer
-            hora
-          </p>
+            <p>
+              Com o Coffee Delivery você recebe seu café onde estiver, a
+              qualquer hora
+            </p>
+          </IntroTitle>
 
-          <AdvantagesItems>
-            <Advantage color="orange">
-              <span>
-                <ShoppingCart weight="fill" size={16} />
-              </span>
-
-              <p>Compra simples e segura</p>
-            </Advantage>
-
-            <Advantage color="gray">
-              <span>
-                <ArchiveBox weight="fill" size={16} />
-              </span>
-
-              <p>Embalagem mantém o café intacto</p>
-            </Advantage>
-
-            <Advantage color="yellow">
-              <span>
-                <Timer weight="fill" size={16} />
-              </span>
-
-              <p>Entrega rápida e rastreada</p>
-            </Advantage>
-
-            <Advantage color="purple">
-              <span>
-                <Coffee weight="fill" size={16} />
-              </span>
-
-              <p>O café chega fresquinho até você</p>
-            </Advantage>
-          </AdvantagesItems>
+          <AdvantageList />
         </Intro>
 
         <img src={CoffeeCup} alt="" />
@@ -99,209 +34,13 @@ export const Home = () => {
         <h1>Nossos cafés</h1>
 
         <CoffeeList>
-          <CoffeeCard>
-            <img src={CoffeeSample} alt="" />
+          <CoffeeCard />
 
-            <span>Tradicional</span>
+          <CoffeeCard />
 
-            <h1>Expresso Tradicional</h1>
+          <CoffeeCard />
 
-            <p>O tradicional café feito com água quente e grãos moídos</p>
-
-            <CoffeeCardFooter>
-              <p>
-                R$ <span>9,90</span>
-              </p>
-
-              <div>
-                <Quantity>
-                  <button>
-                    <Minus size={14} weight="fill" />
-                  </button>
-
-                  <p>1</p>
-
-                  <button>
-                    <Plus size={14} weight="fill" />
-                  </button>
-                </Quantity>
-
-                <button>
-                  <ShoppingCart size={22} weight="fill" />
-                </button>
-              </div>
-            </CoffeeCardFooter>
-          </CoffeeCard>
-
-          <CoffeeCard>
-            <img src={CoffeeSample} alt="" />
-
-            <span>Tradicional</span>
-
-            <h1>Expresso Tradicional</h1>
-
-            <p>O tradicional café feito com água quente e grãos moídos</p>
-
-            <CoffeeCardFooter>
-              <p>
-                R$ <span>9,90</span>
-              </p>
-
-              <div>
-                <Quantity>
-                  <button>
-                    <Minus size={14} weight="fill" />
-                  </button>
-
-                  <p>1</p>
-
-                  <button>
-                    <Plus size={14} weight="fill" />
-                  </button>
-                </Quantity>
-
-                <button>
-                  <ShoppingCart size={22} weight="fill" />
-                </button>
-              </div>
-            </CoffeeCardFooter>
-          </CoffeeCard>
-
-          <CoffeeCard>
-            <img src={CoffeeSample} alt="" />
-
-            <span>Tradicional</span>
-
-            <h1>Expresso Tradicional</h1>
-
-            <p>O tradicional café feito com água quente e grãos moídos</p>
-
-            <CoffeeCardFooter>
-              <p>
-                R$ <span>9,90</span>
-              </p>
-
-              <div>
-                <Quantity>
-                  <button>
-                    <Minus size={14} weight="fill" />
-                  </button>
-
-                  <p>1</p>
-
-                  <button>
-                    <Plus size={14} weight="fill" />
-                  </button>
-                </Quantity>
-
-                <button>
-                  <ShoppingCart size={22} weight="fill" />
-                </button>
-              </div>
-            </CoffeeCardFooter>
-          </CoffeeCard>
-
-          <CoffeeCard>
-            <img src={CoffeeSample} alt="" />
-
-            <span>Tradicional</span>
-
-            <h1>Expresso Tradicional</h1>
-
-            <p>O tradicional café feito com água quente e grãos moídos</p>
-
-            <CoffeeCardFooter>
-              <p>
-                R$ <span>9,90</span>
-              </p>
-
-              <div>
-                <Quantity>
-                  <button>
-                    <Minus size={14} weight="fill" />
-                  </button>
-
-                  <p>1</p>
-
-                  <button>
-                    <Plus size={14} weight="fill" />
-                  </button>
-                </Quantity>
-
-                <button>
-                  <ShoppingCart size={22} weight="fill" />
-                </button>
-              </div>
-            </CoffeeCardFooter>
-          </CoffeeCard>
-
-          <CoffeeCard>
-            <img src={CoffeeSample} alt="" />
-
-            <span>Tradicional</span>
-
-            <h1>Expresso Tradicional</h1>
-
-            <p>O tradicional café feito com água quente e grãos moídos</p>
-
-            <CoffeeCardFooter>
-              <p>
-                R$ <span>9,90</span>
-              </p>
-
-              <div>
-                <Quantity>
-                  <button>
-                    <Minus size={14} weight="fill" />
-                  </button>
-
-                  <p>1</p>
-
-                  <button>
-                    <Plus size={14} weight="fill" />
-                  </button>
-                </Quantity>
-
-                <button>
-                  <ShoppingCart size={22} weight="fill" />
-                </button>
-              </div>
-            </CoffeeCardFooter>
-          </CoffeeCard>
-
-          <CoffeeCard>
-            <img src={CoffeeSample} alt="" />
-
-            <span>Tradicional</span>
-
-            <h1>Expresso Tradicional</h1>
-
-            <p>O tradicional café feito com água quente e grãos moídos</p>
-
-            <CoffeeCardFooter>
-              <p>
-                R$ <span>9,90</span>
-              </p>
-
-              <div>
-                <Quantity>
-                  <button>
-                    <Minus size={14} weight="fill" />
-                  </button>
-
-                  <p>1</p>
-
-                  <button>
-                    <Plus size={14} weight="fill" />
-                  </button>
-                </Quantity>
-
-                <button>
-                  <ShoppingCart size={22} weight="fill" />
-                </button>
-              </div>
-            </CoffeeCardFooter>
-          </CoffeeCard>
+          <CoffeeCard />
         </CoffeeList>
       </CoffeeListContainer>
     </div>
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,67 +1,5 @@
 import { styled } from 'styled-components'
 
-export const HeaderContainer = styled.header`
-  width: 100vw;
-
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-
-  padding: 2rem 0;
-
-  max-width: 1120px;
-  margin: 0 auto;
-
-  nav {
-    display: flex;
-    align-items: center;
-
-    gap: 0.75rem;
-  }
-`
-
-export const Location = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  gap: 0.25rem;
-
-  height: 38px;
-
-  border-radius: 6px;
-  background: #ebe5f9;
-  color: #4b2995;
-
-  padding: 0 0.5rem;
-
-  font-family: 'Roboto', sans-serif;
-
-  font-size: 0.75rem;
-  font-weight: 400;
-
-  svg {
-    color: #8047f8;
-  }
-`
-
-export const ShoppingCartButton = styled.button`
-  border: none;
-
-  width: 38px;
-  height: 38px;
-
-  display: flex;
-  align-items: center;
-  justify-content: center;
-
-  border-radius: 6px;
-
-  background: #f1e9c9;
-  color: #c47f17;
-
-  cursor: pointer;
-`
-
 export const IntroSectionContainer = styled.section`
   display: grid;
   grid-template-columns: 1.25fr 1fr;
@@ -74,80 +12,40 @@ export const IntroSectionContainer = styled.section`
 
   max-width: 1120px;
   margin: 0 auto;
+`
 
-  h1 {
+export const Intro = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 4rem;
+`
+
+export const IntroTitle = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  gap: 1rem;
+
+  > h1 {
     font-size: 48px;
     font-weight: bold;
     line-height: 130%;
     color: #272221;
   }
 
-  p {
+  > p {
     font-size: 20px;
     color: #403937;
   }
 `
 
-export const Intro = styled.div`
-  display: flex;
-  flex-direction: column;
-`
-
-export const AdvantagesItems = styled.div`
-  flex: 1;
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-
-  margin-top: 4.125rem;
-  row-gap: 1.5rem;
-`
-
-const ADVANTAGE_COLORS = {
-  orange: '#C47F17',
-  gray: '#574F4D',
-  yellow: '#DBAC2C',
-  purple: '#8047F8',
-} as const
-
-interface AdvantageProps {
-  color: keyof typeof ADVANTAGE_COLORS
-}
-
-export const Advantage = styled.div<AdvantageProps>`
-  display: flex;
-  gap: 0.75rem;
-  align-items: center;
-
-  span {
-    width: 32px;
-    height: 32px;
-    border-radius: 999px;
-    background: ${({ color }) => ADVANTAGE_COLORS[color]};
-    display: flex;
-    align-items: center;
-    justify-content: center;
-
-    svg {
-      color: #fff;
-    }
-  }
-
-  p {
-    font-family: 'Roboto', sans-serif;
-    line-height: 130%;
-    font-size: 16px;
-    color: #574f4d;
-  }
-`
-
 export const CoffeeListContainer = styled.div`
-  max-width: 1120px;
   margin: 0 auto;
   display: flex;
   flex-direction: column;
   gap: 3.375rem;
 
-  padding: 2rem 0 9.75rem;
+  padding: 2rem 10rem 0;
 
   h1 {
     color: #403937;
@@ -160,126 +58,4 @@ export const CoffeeList = styled.div`
   grid-template-columns: repeat(4, 1fr);
   row-gap: 2.5rem;
   column-gap: 2rem;
-`
-
-export const CoffeeCard = styled.div`
-  background: #f3f2f2;
-  border-radius: 6px 36px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-
-  padding: 1.25rem;
-
-  img {
-    margin-top: calc(-1.5rem - 20px);
-    margin-bottom: 0.75rem;
-  }
-
-  > span {
-    font-family: 'Roboto', sans-serif;
-    padding: 0.25rem 0.5rem;
-    border-radius: 999px;
-    background: #f1e9c9;
-    color: #c47f17;
-    font-size: 0.625rem;
-    line-height: 130%;
-    font-weight: 700;
-
-    text-transform: uppercase;
-
-    margin-bottom: 1rem;
-  }
-
-  h1 {
-    color: #403937;
-    font-weight: 700;
-    font-size: 1.25rem;
-    margin-bottom: 0.5rem;
-  }
-
-  > p {
-    text-align: center;
-    font-family: 'Roboto', sans-serif;
-    color: #8d8686;
-    font-size: 0.875rem;
-    margin-bottom: 2rem;
-  }
-`
-
-export const CoffeeCardFooter = styled.div`
-  width: 100%;
-  display: flex;
-
-  align-items: center;
-  justify-content: space-between;
-
-  > div {
-    display: flex;
-    gap: 0.5rem;
-  }
-
-  p {
-    color: #574f4d;
-    line-height: 130%;
-    font-size: 0.875rem;
-    font-family: 'Roboto', sans-serif;
-
-    span {
-      font-weight: 700;
-      font-size: 1.5rem;
-    }
-  }
-
-  button {
-    width: 38px;
-    height: 38px;
-    border-radius: 6px;
-    color: #fff;
-    background: #4b2995;
-
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    cursor: pointer;
-  }
-`
-
-export const Quantity = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-
-  height: 38px;
-
-  background: #e6e5e5;
-  border-radius: 6px;
-  padding: 0 0.5rem;
-
-  gap: 4px;
-
-  p {
-    width: 20px;
-    color: #272221;
-    line-height: 130%;
-    font-size: 1rem;
-    font-family: 'Roboto', sans-serif;
-    text-align: center;
-  }
-
-  > button {
-    border: none;
-    width: 14px;
-    height: 14px;
-    color: #8047f8;
-    background: transparent;
-
-    display: flex;
-    align-items: center;
-    justify-content: center;
-
-    cursor: pointer;
-
-    font-weight: 700;
-  }
 `
