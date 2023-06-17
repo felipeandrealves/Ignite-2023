9682f5ea189473269bd69a847cfa8233c190b30c
Felipe Alves <felipe_alves_sbo@hotmail.com>
2023-06-16 23:11:10 -0300

    feat: add success page

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom'
 
 import { DefaultLayout } from './common/layouts/DefaultLayout'
 import { Checkout } from './pages/Checkout'
+import { Success } from './pages/Success'
 import { Home } from './pages/Home'
 
 export const Router = () => {
@@ -11,6 +12,8 @@ export const Router = () => {
         <Route path="/" element={<Home />} />
 
         <Route path="/checkout" element={<Checkout />} />
+
+        <Route path="/checkout/success" element={<Success />} />
       </Route>
     </Routes>
   )
