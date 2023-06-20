77d02044adcc4ff0a6a4ea1a84dfc891db414ee4
Felipe Alves <felipe_alves_sbo@hotmail.com>
2023-06-19 22:32:35 -0300

    fix: create cart functions

--- src/common/contexts/CoffeeContext.tsx
+++ src/common/contexts/CoffeeContext.tsx
@@ -1,15 +1,23 @@
-import { ReactNode, createContext, useContext, useState } from 'react'
+import {
+  ReactNode,
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+} from 'react'
 
 import { coffees } from '../data/coffees.json'
 
 export type Coffee = (typeof coffees)[0]
 
-type CoffeeCart = Coffee & { amount: number }
+export type CoffeeCart = Coffee & { amount: number }
 
 interface CoffeeContextData {
   availableCoffees: Coffee[]
   coffeeCart: CoffeeCart[]
+  totalCartValue: number
   addCoffeeToCart: (coffee: CoffeeCart) => void
+  removeCoffeeFromCart: (coffeeId: string) => void
 }
 
 export const CoffeeContext = createContext({} as CoffeeContextData)
@@ -23,6 +31,7 @@ export const CoffeeContextProvider = ({
 }: CoffeeContextProviderProps) => {
   const [availableCoffees] = useState<typeof coffees>(coffees)
   const [coffeeCart, setCoffeeCart] = useState<CoffeeCart[]>([])
+  const [totalCartValue, setTotalCartValue] = useState(0)
 
   const addCoffeeToCart = (coffee: CoffeeCart) => {
     const coffeeAlreadyExists = coffeeCart.find((item) => item.id === coffee.id)
@@ -33,14 +42,43 @@ export const CoffeeContextProvider = ({
           item.id === coffee.id ? { ...coffee, amount: item.amount } : item,
         ),
       )
-    } else {
-      setCoffeeCart([...coffeeCart, { ...coffee }])
+    } else setCoffeeCart([...coffeeCart, { ...coffee }])
+  }
+
+  const removeCoffeeFromCart = (coffeeId: string) => {
+    const coffeeAlreadyExists = coffeeCart.find((item) => item.id === coffeeId)
+
+    if (coffeeAlreadyExists) {
+      setCoffeeCart(coffeeCart.filter((item) => item.id !== coffeeId))
     }
   }
 
+  useEffect(() => {
+    if (coffeeCart.length !== 0) {
+      const totalAmount = coffeeCart.reduce((acc, item) => {
+        return acc + item.amount * item.price
+      }, 0)
+
+      setTotalCartValue(totalAmount)
+    }
+
+    const jsonCoffeeCart = JSON.stringify(coffeeCart)
+
+    localStorage.setItem(
+      '@ignite-coffeeDelivery:coffee-cart-state-1.0.0',
+      jsonCoffeeCart,
+    )
+  }, [coffeeCart])
+
   return (
     <CoffeeContext.Provider
-      value={{ availableCoffees, coffeeCart, addCoffeeToCart }}
+      value={{
+        availableCoffees,
+        coffeeCart,
+        addCoffeeToCart,
+        totalCartValue,
+        removeCoffeeFromCart,
+      }}
     >
       {children}
     </CoffeeContext.Provider>
