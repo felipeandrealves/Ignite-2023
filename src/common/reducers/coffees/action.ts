3abeaf93b56742024248caa1742c8a2b588b1a0c
Felipe Alves <felipe_alves_sbo@hotmail.com>
2023-06-24 10:28:53 -0300

    feat: create new order actions

--- src/common/reducers/coffees/action.ts
+++ src/common/reducers/coffees/action.ts
@@ -1,9 +1,12 @@
-import { Coffee } from './reducer'
+import { Address, Coffee, Order } from './reducer'
 
 export enum ActionTypes {
   ADD_COFFEE_TO_CART = 'ADD_NEW_COFFEE',
   CHANGE_COFFEE_AMOUNT = 'CHANGE_COFFEE_AMOUNT',
   REMOVE_COFFEE = 'REMOVE_COFFEE',
+  CLEAR_CART = 'CLEAR_CART',
+  ADD_NEW_ORDER = 'ADD_NEW_ORDER',
+  CHANGE_ADDRESS = 'CHANGE_ADDRESS',
 }
 
 export const addCoffeeToCartAction = (newCoffee: Coffee) => {
@@ -30,6 +33,30 @@ export const removeCoffeeAction = (coffeeId: string) => {
     type: ActionTypes.REMOVE_COFFEE,
     payload: {
       coffeeId,
+    },
+  }
+}
+
+export const clearCartAction = () => {
+  return {
+    type: ActionTypes.CLEAR_CART,
+  }
+}
+
+export const addNewOrderAction = (newOrder: Order) => {
+  return {
+    type: ActionTypes.ADD_NEW_ORDER,
+    payload: {
+      newOrder,
+    },
+  }
+}
+
+export const changeAddressAction = (address: Address) => {
+  return {
+    type: ActionTypes.CHANGE_ADDRESS,
+    payload: {
+      address,
     },
   }
 }
