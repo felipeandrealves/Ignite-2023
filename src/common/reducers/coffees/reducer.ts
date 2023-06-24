11da90388b3f6026bae18c77007bb01b35914a87
Felipe Alves <felipe_alves_sbo@hotmail.com>
2023-06-24 10:29:07 -0300

    feat: create new order cases

--- src/common/reducers/coffees/reducer.ts
+++ src/common/reducers/coffees/reducer.ts
@@ -7,8 +7,32 @@ export type CoffeeData = (typeof coffees)[0]
 
 export type Coffee = CoffeeData & { amount: number }
 
+export type Address = {
+  street: string
+  number: string
+  city: string
+  state: string
+  zipCode: string
+
+  complement?: string
+}
+
+export type Order = {
+  id: string
+  coffees: Coffee[]
+  address: Address
+  total: number
+
+  paymentType: 'creditCard' | 'debitCard' | 'cash'
+
+  status: 'pending' | 'completed' | 'cancelled'
+  createdAt: number
+}
+
 interface CoffeeState {
   coffeeCart: Coffee[]
+  orders: Order[]
+  address?: Address
 }
 
 export const coffeeReducer = (state: CoffeeState, action: any) => {
@@ -52,7 +76,34 @@ export const coffeeReducer = (state: CoffeeState, action: any) => {
       })
     }
 
+    case ActionTypes.CLEAR_CART:
+      return produce(state, (draftState) => {
+        draftState.coffeeCart = []
+      })
+
+    case ActionTypes.ADD_NEW_ORDER:
+      return produce(state, (draftState) => {
+        draftState.orders.push(action.payload.newOrder)
+      })
+
+    case ActionTypes.CHANGE_ADDRESS:
+      return produce(state, (draftState) => {
+        draftState.address = action.payload.address
+      })
+
     default:
       return state
   }
+}
+
+export const initialState = (initialState: CoffeeState): CoffeeState => {
+  const storedStateAsJson = localStorage.getItem(
+    '@ignite-coffee-delivery:coffee-state-1.0.0',
+  )
+
+  if (storedStateAsJson) {
+    return JSON.parse(storedStateAsJson)
+  }
+
+  return initialState
 }
