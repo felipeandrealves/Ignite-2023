98e0e25531b5adc74e9670036fc53662a980f99b
Felipe Alves <felipe_alves_sbo@hotmail.com>
2023-06-28 22:48:17 -0300

    feat: create transaction search form

---
+++ src/pages/transaction/components/SearchForm/styles.ts
@@ -0,0 +1,42 @@
+import { styled } from 'styled-components'
+
+export const SearchFormContainer = styled.form`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 1rem;
+
+  input {
+    flex: 1;
+    border-radius: 6px;
+    border: 0;
+    background: ${({ theme }) => theme['gray-900']};
+    color: ${({ theme }) => theme['gray-300']};
+    padding: 1rem;
+
+    &::placeholder {
+      color: ${({ theme }) => theme['gray-500']};
+    }
+  }
+
+  button {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    padding: 1rem;
+    background: transparent;
+    border: 1px solid ${({ theme }) => theme['green-300']};
+    border-radius: 6px;
+    color: ${({ theme }) => theme['green-300']};
+    font-weight: 700;
+    transition: all 0.2s;
+    cursor: pointer;
+
+    &:hover {
+      background: ${({ theme }) => theme['green-500']};
+      border: 1px solid ${({ theme }) => theme['green-500']};
+      color: ${({ theme }) => theme.white};
+    }
+  }
+`
