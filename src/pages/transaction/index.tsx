188b88661a8e1360819880f76c26c382198e9e1e
Felipe Alves <felipe_alves_sbo@hotmail.com>
2023-06-28 21:54:21 -0300

    feat: create transaction page

---
+++ src/pages/transaction/index.tsx
@@ -0,0 +1,9 @@
+import { TransactionContainer } from './styles'
+
+export const Transaction = () => {
+  return (
+    <TransactionContainer>
+      <h1>Transaction</h1>
+    </TransactionContainer>
+  )
+}
