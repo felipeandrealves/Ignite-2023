59611e8a15c99a698e1cc00ccd92e7576d8e08f8
Felipe Alves <felipe_alves_sbo@hotmail.com>
2023-06-29 23:09:34 -0300

    fix: correct folder name

---
+++ src/pages/transactions/index.tsx
@@ -0,0 +1,45 @@
+import {
+  PriceHighlight,
+  TransactionContainer,
+  TransactionContent,
+  TransactionsTable,
+} from './styles'
+import { useTransactions } from '../../contexts/TransactionsContext'
+import { currencyFormatter, dateFormatter } from '../../utils/formatter'
+import { SearchForm } from './components/SearchForm'
+import { Summary } from '../../components/Summary'
+import { Header } from '../../components/Header'
+
+export const Transactions = () => {
+  const { transactions } = useTransactions()
+
+  return (
+    <TransactionContainer>
+      <Header />
+
+      <Summary />
+
+      <TransactionContent>
+        <SearchForm />
+
+        <TransactionsTable>
+          <tbody>
+            {transactions.map((transaction) => (
+              <tr key={transaction.id}>
+                <td width="50%">{transaction.description}</td>
+                <td>
+                  <PriceHighlight variant={transaction.type}>
+                    {transaction.type === 'outcome' && '- '}
+                    {currencyFormatter.format(transaction.price)}
+                  </PriceHighlight>
+                </td>
+                <td>{transaction.category}</td>
+                <td>{dateFormatter.format(new Date(transaction.createdAt))}</td>
+              </tr>
+            ))}
+          </tbody>
+        </TransactionsTable>
+      </TransactionContent>
+    </TransactionContainer>
+  )
+}
