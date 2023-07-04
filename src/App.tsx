aed30bad58a08b00b3a953fa6c66cea0f0d91da7
Felipe Alves <felipe_alves_sbo@hotmail.com>
2023-07-03 22:50:39 -0300

    feat: app providers

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import { ThemeProvider } from 'styled-components'
+import { BrowserRouter } from 'react-router-dom'
+
+import { defaultTheme } from './theme/default'
+import { GlobalStyle } from './styles/global'
+import { Router } from './Router'
+
 export const App = () => {
   return (
-    <div>
-      <h1>App Home</h1>
-    </div>
+    <BrowserRouter>
+      <ThemeProvider theme={defaultTheme}>
+        <GlobalStyle />
+
+        <Router />
+      </ThemeProvider>
+    </BrowserRouter>
   )
 }
