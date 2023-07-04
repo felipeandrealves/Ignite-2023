8dbdbb0e1fd86505024cf89cf2a7c7705e5bc4fe
Felipe Alves <felipe_alves_sbo@hotmail.com>
2023-07-03 22:52:29 -0300

    feat: create post route

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,12 +2,15 @@ import { Route, Routes } from 'react-router-dom'
 
 import { DefaultLayout } from './layout/DefaultLayout'
 import { Home } from './pages/Home'
+import { Post } from './pages/Post'
 
 export const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<DefaultLayout />}>
         <Route path="/" element={<Home />} />
+
+        <Route path="/post/:id" element={<Post />} />
       </Route>
     </Routes>
   )
